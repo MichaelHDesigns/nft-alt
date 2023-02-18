bdb245e83e75ddfd518a023245fc7c986324e69c
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-02-17 23:10:15 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,35 @@ import type { AppType } from "next/dist/shared/lib/utils";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import AuthProvider from "../components/AuthProvider";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Altcoinchain;
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider desiredChainId={ChainId.Altcoinchain}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+    <ThirdwebProvider
+      chainRpc={{
+        [ChainId.Altcoinchain]:
+          `https://rpc0.altcoinchain.org/rpc`,
+      }}
+      desiredChainId={activeChainId}
+    >
+      <Head>
+        <title>thirdweb Marketplace with Next.JS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Learn How To Use Thirdweb's Marketplace with Next.JS To List Your NFTs For Sale, Accept Bids, and Buy NFTs"
+        />
+        <meta
+          name="keywords"
+          content="Thirdweb, Marketplace, NFT Marketplace Tutorial, NFT Auction Tutorial, How To Make OpenSea"
+        />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+      {/* <ThirdwebGuideFooter /> */}
     </ThirdwebProvider>
   );
-};
+}
 
 export default MyApp;
