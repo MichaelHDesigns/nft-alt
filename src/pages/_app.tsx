1893099b05969904a7a0c8fa546bcffb79e26e60
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-02-18 00:16:50 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,9 @@ import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import AuthProvider from "../components/AuthProvider";
-import { ChainId, Altcoinchain } from "@thirdweb-dev/chains";
+//import { ChainId, Altcoinchain } from "@thirdweb-dev/chains";
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-
+import { ChainId } from "@thirdweb-dev/chains/2330.ts";
 const activeChainId = ChainId.Altcoinchain;
 
 const MyApp: AppType = ({ Component, pageProps }) => {
