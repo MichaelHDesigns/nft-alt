b89704de6bfad4b681b32c9c435bb6116d5d5650
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-02-17 23:54:57 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,12 @@ import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import AuthProvider from "../components/AuthProvider";
-//import { ChainId } from "@thirdweb-dev/chains";
+import { ChainId } from "@thirdweb-dev/chains";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-   <ThirdwebProvider 
-     activeChain={{
+   <ThirdwebProvider
+      activeChain={{
         // === Required information for connecting to the network === \\
         chainId: 2330, // Chain ID of the network
         // Array of RPC URLs to use
@@ -17,11 +17,16 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         // Information about the chains native currency (i.e. the currency that is used to pay for gas)
         nativeCurrency: {
           decimals: 18,
-          name: "Altcoinchain",
+          name: "Altcoin",
           symbol: "ALT",
         },
+        shortName: "alt", // Display value shown in the wallet UI
+        slug: "altcoinchain", // Display value shown in the wallet UI
+        testnet: false, // Boolean indicating whether the chain is a testnet or mainnet
+        chain: "mainnet", // Name of the network
+        name: "Altcoin", // Name of the network
       }}
-     >
+    >
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
