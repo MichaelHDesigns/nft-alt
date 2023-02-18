e9b0b15ab640c027e65d5345ea99ceb53a228a47
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-02-17 23:47:34 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,22 @@ import { ChainId } from "@thirdweb-dev/chains";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-   <ThirdwebProvider desiredChainId={ChainId.Altcoinchain}>
+   <ThirdwebProvider 
+     activeChain={{
+        // === Required information for connecting to the network === \\
+        chainId: 2330, // Chain ID of the network
+        // Array of RPC URLs to use
+        rpc: ["https://rpc0.altcoinchain.org/rpc"],
+
+        // === Information for adding the network to your wallet (how it will appear for first time users) === \\
+        // Information about the chains native currency (i.e. the currency that is used to pay for gas)
+        nativeCurrency: {
+          decimals: 18,
+          name: "Altcoinchain",
+          symbol: "ALT",
+        },
+      }}
+     >
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
